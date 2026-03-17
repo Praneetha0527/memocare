ebc6fcb16eb2a75cc3631d73985409393eac79aa
Praneetha0527 <praneetha2227@gmail.com>
2026-03-17 13:41:01 +0530

    Triggered Workflow

--- backend/routes/voicenotes.js
+++ backend/routes/voicenotes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const VoiceNote = require("../models/VoiceNote");
 
-// GET all notes
 router.get("/", async (req, res) => {
   try {
     const notes = await VoiceNote.find().sort({ date: -1 });
